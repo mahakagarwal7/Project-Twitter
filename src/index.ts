43ba66c64702f8fb38ee485662400cd442ad14be
KrishLalakiya <krishrlalakiya@gmail.com>
2026-01-08 23:57:00 +0530

    adding redis

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 // src/index.ts
 import { app } from "./app";
+import redisClient, { connectRedis } from "./config/redis";
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+(async () => {
+  try {
+    await connectRedis();
+    // await redisClient.set("health", "ok");
+    // console.log("Redis health:", await redisClient.get("health")); 
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Startup failed:", error);
+    process.exit(1);
+  }
+})();
